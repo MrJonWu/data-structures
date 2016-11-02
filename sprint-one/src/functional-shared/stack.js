4e59257eb1910e36adf4f3c19491c4d711650724
MrJonWu <mrjonwu@gmail.com>
2016-11-02 13:25:33 -0700

    Get started on functional shared classes

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  extend(instance, stackMethods);
+  return instance;
+  
 };
-
 var stackMethods = {};
+stackMethods.push = function() {
+
+}
+stackMethods.pop = function() {
+
+}
+stackMethods.size = function() {
+
+}
 
 
