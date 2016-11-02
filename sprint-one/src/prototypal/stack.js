ceb8c457211be39337f63d53f9f8c5f308fb247e
MrJonWu <mrjonwu@gmail.com>
2016-11-02 15:26:42 -0700

    Make stack work for Prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  storage: {},
+  siz: 0,
+  push: function(val) {
+    this.siz++;
+    this.storage[this.siz] = val;
+  },
+  pop: function() {
+    var val = this.storage[this.siz];
+    delete this.storage[this.siz];
+    if (this.siz > 0) {
+      this.siz--;
+    }
+    return val;
+  },
+  size: function() {
+    return this.siz;
+  }
+};
 
 
