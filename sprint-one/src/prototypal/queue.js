3f9a3d2140224ba4a5b1b8c4f1131bf04dc01547
MrJonWu <mrjonwu@gmail.com>
2016-11-02 15:33:33 -0700

    Make queue work for Prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  storage: {},
+  siz: 0,
+  enqueue: function(val) {
+    this.siz++;
+    this.storage[this.siz] = val;
+  },
+  dequeue: function() {
+    var val = this.storage[1];
+    if (this.siz > 1) {
+      for (var i = 1; i <= this.siz; i++) {
+        this.storage[i] = this.storage[i + 1];
+      }
+    }
+    delete this.storage[this.siz];
+    if (this.siz > 0) {
+      this.siz--;
+    }
+    return val;
+  },
+  size: function() {
+    return this.siz;
+  }
+};
 
 
