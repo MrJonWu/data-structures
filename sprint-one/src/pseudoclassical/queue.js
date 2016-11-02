613ee6e33a2a7fe8b4fa7ae0790f1a00a7f202b4
MrJonWu <mrjonwu@gmail.com>
2016-11-02 15:39:48 -0700

    Make Stack and Queue qwork for pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.siz = 0;
+};
+
+Queue.prototype.enqueue = function(val) {
+  this.siz++;
+  this.storage[this.siz] = val;
+};
+Queue.prototype.dequeue = function() {
+  var val = this.storage[1];
+  if (this.siz > 1) {
+    for (var i = 1; i <= this.siz; i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+  }
+  delete this.storage[this.siz];
+  if (this.siz > 0) {
+    this.siz--;
+  }
+  return val;
+};
+Queue.prototype.size = function() {
+  return this.siz;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.siz = 0;
 };
 
-
+Stack.prototype.push = function(val) {
+  this.siz++;
+  this.storage[this.siz] = val;
+};
+Stack.prototype.pop = function() {
+  var val = this.storage[this.siz];
+  delete this.storage[this.siz];
+  if (this.siz > 0) {
+    this.siz--;
+  }
+  return val;
+};
+Stack.prototype.size = function() {
+  return this.siz;
+};
