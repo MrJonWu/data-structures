0bd7ac7216d57461f1e2350992928b4bc8e8f1a7
MrJonWu <mrjonwu@gmail.com>
2016-11-02 14:56:27 -0700

    Make stack work for functional share pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,19 +2,34 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var instance = {};
+  instance.storage = {};
+  instance.siz = 0;
   extend(instance, stackMethods);
   return instance;
   
 };
+var extend = function(arg, arg2) {
+  for (var key in arg2) {
+    arg[key] = arg2[key];
+  }
+};
+
 var stackMethods = {};
-stackMethods.push = function() {
-
-}
+stackMethods.push = function(val) {
+  this.siz++;
+  this.storage[this.siz] = val;
+};
 stackMethods.pop = function() {
+  var val = this.storage[this.siz];
+  delete this.storage[this.siz];
+  if (this.siz > 0) {
+    this.siz--;
+  }
+  return val;
 
-}
+};
 stackMethods.size = function() {
-
-}
+  return this.siz;
+};
 
 
