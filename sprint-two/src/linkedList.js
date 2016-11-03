99410e709ebc9c3f4b256dbd2e2ff141d0f8f424
MrJonWu <mrjonwu@gmail.com>
2016-11-02 20:13:57 -0700

    Make all aspects of linkedList work except for contain

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,30 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var nod = Node(value);
+    if (!list.head) {
+      list.head = nod;
+    }
+    if (list.tail) {
+      list.tail.next = nod;
+    }
+
+    list.tail = nod;
   };
 
   list.removeHead = function() {
+    var val = list.head.value;
+    list.head = list.head.next;
+    return val;
   };
 
   list.contains = function(target) {
+  //   for (key in list) {
+  //     if (key.value) {
+  //       return true;
+  //     }
+  //   }
+  //   return false;
   };
 
   return list;
