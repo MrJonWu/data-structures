6a0a08a62fe4997ebff60d0c4711419aacd390ee
MrJonWu <mrjonwu@gmail.com>
2016-11-03 11:16:40 -0700

    Make set.js pass all tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,28 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this._storage) {
+    this._storage = [];
+  }
+  if (!_.contains(this._storage, item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage) {
+    return _.contains(this._storage, item);
+  }
 };
 
 setPrototype.remove = function(item) {
+  if (this._storage) {
+    for (var i = 0; i < this._storage.length; i++) {
+      if (this._storage[i] === item) {
+        this._storage.splice(i, 1);
+      }
+    }
+  }
 };
 
 /*
