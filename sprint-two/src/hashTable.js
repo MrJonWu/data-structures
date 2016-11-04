0c09a2eaa59b2fc4471bdef3077ca8a1a061fba3
MrJonWu <mrjonwu@gmail.com>
2016-11-03 20:40:41 -0700

    refactor hashTable.js & rm log from treeSpec.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,44 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[k] = [v, index];
+  if (this._storage.get(index)) { //if there is a value at index
+    var temp = this._storage.get(index);
+    var hasK = false;
+    for (var i = 0; i < temp.length; i++) {
+      if (temp[i][0] === k) {
+        temp[i][1] = v;
+        hasK = true;
+      }
+    }
+    if (!hasK) {
+      temp.push([k, v]);
+    }
+    this._storage.set(index, temp);
+  } else {
+    this._storage.set(index, [[k, v]]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage[k]) {
-    return this._storage[k][0];
+  var temp = this._storage.get(index);
+  for (var i = 0; i < temp.length; i++) {
+    if (temp[i][0] === k) {
+      return temp[i][1];
+    }
   }
-  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  delete this._storage[k];
+  this._storage.each(function(storageInd, y, storage) {
+    for (var i = 0; i < storage[index].length; i++) {
+      console.log(storage[index][i]);
+      if (storage[index][i][0] === k) {
+        storage[index].splice(i, 1);
+      }
+    }
+  });
 };
 
 
